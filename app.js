b54497b5f378c1d9e4069252c198142c66fb2457
Amine ALOULOU <amine.aloulou@nordnet.com>
2021-11-19 02:05:19 +0100

    pretty print result and exit process.

--- app.js
+++ app.js
@@ -7,11 +7,11 @@
  */
 import { data } from './data.cjs';
 import {
-    COUNT_ARG_NAME,
-    COUNT_SHORT_ARG_NAME,
-    FILTER_ARG_NAME,
-    FILTER_SHORT_ARG_NAME,
-    HELP_MESSAGE
+	COUNT_ARG_NAME,
+	COUNT_SHORT_ARG_NAME,
+	FILTER_ARG_NAME,
+	FILTER_SHORT_ARG_NAME,
+	HELP_MESSAGE
 } from './const.js';
 import { count } from './count.js';
 import { filter } from './filter.js';
@@ -23,19 +23,32 @@ import { readArgs } from './read-args.js';
  * An entry function for cli. Magic happens here.
  */
 function main() {
-    // reading arguments
-    const { option, value } = readArgs();
-    let result;
-    if ([COUNT_ARG_NAME, COUNT_SHORT_ARG_NAME].includes(option)) {
-        // count option
-        result = count(data);
-    } else if ([FILTER_ARG_NAME, FILTER_SHORT_ARG_NAME].includes(option)) {
-        // filter option
-        result = filter(data, value);
-    } else {
-        // display help 
-        console.log(HELP_MESSAGE)
-    }
+
+	try {
+		// reading arguments
+		const { option, value } = readArgs();
+		let result;
+		if ([COUNT_ARG_NAME, COUNT_SHORT_ARG_NAME].includes(option)) {
+			// count option
+			result = count(data);
+		} else if ([FILTER_ARG_NAME, FILTER_SHORT_ARG_NAME].includes(option)) {
+			// filter option
+			result = filter(data, value);
+		} else {
+			// display help 
+			console.log(HELP_MESSAGE);
+			return;
+		}
+		// pretty print output
+		console.log(JSON.stringify(result, null, 2))
+		// exit process with success.
+		process.exit(0);
+	} catch (error) {
+		// log error to console and send a non zero exit
+		const { message } = error;
+		console.error(message ? message : error);
+		process.exit(1);
+	}
 }
 
 // invoke entry point.
